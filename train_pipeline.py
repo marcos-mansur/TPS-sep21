e4c57575632fa4edd940eee4044a710a45c9ea43
Thiago Ouverney <thiagouverney@gmail.com>
2021-09-23 15:42:04 -0300

    train_pipeline.py fix

--- train_pipeline.py
+++ train_pipeline.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from skelearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score
 from Functions import (seed_everything,
                        preprocess,
                        manual_cross,
@@ -17,7 +17,7 @@ df = pd.read_csv(r'Data/train_sampled_red.csv',index_col='id')
 x = df.drop('claim', axis=1)
 y= df.claim
 #split the data
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_seed=RANDOM_SEED)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state= RANDOM_SEED)
 
 pipe_baseline.fit_transform(x_train,y_train)
 pred_baseline = pipe_baseline.predict_proba(x_test)
