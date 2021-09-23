c8b8218d8278901bf7a654c4801c5d4a140809cd
Thiago Ouverney <thiagouverney@gmail.com>
2021-09-23 16:38:40 -0300

    Pipeline best_params

--- Pipelines.py
+++ Pipelines.py
@@ -3,7 +3,24 @@ from sklearn.pipeline import make_pipeline
 from sklearn.impute import SimpleImputer
 from Functions import preprocess
 from lightgbm import LGBMClassifier
+import numpy as np
+
 RANDOM_SEED= 42
+
+
+best_lgbm_params= {
+    'num_leaves': 635,
+    "max_depth": 11,
+    'min_data_in_leaf': 690,
+    'learning_rate': 0.02270155819774826,
+    'n_estimators': 806,
+    'lambda_l1': 21,
+    'lambda_l2': 11,
+    'min_gain_to_split': 2.0362294407676766,
+    'bagging_fraction': 0.6496364626925781,
+    'bagging_freq': 2,
+    'random_state': 42}
+
 
 pipe_baseline = make_pipeline(LGBMClassifier(random_state=RANDOM_SEED))
 
@@ -12,7 +29,7 @@ pipe_preprocess = make_pipeline(preprocess(sum_missing=True, st_dev=True),
                                               strategy='most_frequent',
                                               add_indicator=True))
 pipe_lgbm = make_pipeline(pipe_preprocess,
-                          LGBMClassifier(**best_params_lgbm))
+                          LGBMClassifier(**best_lgbm_params))
 
 
 # AGORA VAI
