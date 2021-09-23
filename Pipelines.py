e7cbd756d6387387de90212711a19b0fbfd1004a
Thiago Ouverney <thiagouverney@gmail.com>
2021-09-23 16:15:21 -0300

    test

--- Pipelines.py
+++ Pipelines.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.pipeline import make_pipeline
-from sklearn.imputer import SimpleImputer
+from sklearn.impute import SimpleImputer
 from Functions import preprocess
 from lightgbm import LGBMClassifier
 RANDOM_SEED= 42
@@ -13,3 +13,6 @@ pipe_preprocess = make_pipeline(preprocess(sum_missing=True, st_dev=True),
                                               add_indicator=True))
 pipe_lgbm = make_pipeline(pipe_preprocess,
                           LGBMClassifier(**best_params_lgbm))
+
+
+# AGORA VAI
